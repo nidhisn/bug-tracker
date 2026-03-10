d40b5ef5460135008896a95b91ec138a4fc266a9
nidhisn <nidhi24sn@gmail.com>
2026-03-10 20:23:13 +0530

    Add homepage and deployment scripts; update devDependencies in package.json and improve bug loading state in App.js

--- src/App.js
+++ src/App.js
@@ -33,17 +33,23 @@ function App() {
         </thead>
 
         <tbody>
-          {bugs.map((bug, index) => (
-            <tr key={index}>
-              <td>{bug.Bug_ID}</td>
-              <td>{bug.Title}</td>
-              <td>{bug.Module}</td>
-              <td>{bug.Priority}</td>
-              <td>{bug.Status}</td>
-              <td>{bug.Assigned_To}</td>
-              <td>{bug.Reported_By}</td>
+          {bugs.length === 0 ? (
+            <tr>
+              <td colSpan="7">Loading bugs...</td>
             </tr>
-          ))}
+          ) : (
+            bugs.map((bug, index) => (
+              <tr key={index}>
+                <td>{bug.Bug_ID}</td>
+                <td>{bug.Title}</td>
+                <td>{bug.Module}</td>
+                <td>{bug.Priority}</td>
+                <td>{bug.Status}</td>
+                <td>{bug.Assigned_To}</td>
+                <td>{bug.Reported_By}</td>
+              </tr>
+            ))
+          )}
         </tbody>
       </table>
     </div>
